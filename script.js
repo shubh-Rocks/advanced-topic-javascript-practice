66726e7f6af9f2bc42e69f2a635b5b4acd0825ef
shubh mishra <smishra64310@gmail.com>
2026-07-18 22:10:08 +0530

    practice the question of loop concepts

--- script.js
+++ script.js
@@ -894,3 +894,14 @@
 
 //   return answer;
 // };
+
+let num = 12348;
+
+let reverse = 0;
+
+while (num > 0) {
+  digit = num % 10;
+  reverse = reverse * 10 + digit;
+  num = Math.floor(num / 10);
+}
+console.log(reverse);

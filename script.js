7aea767c37951605f962e69cbdc59a7880c94e29
shubh mishra <smishra64310@gmail.com>
2026-07-24 21:19:13 +0530

    practice the revarsal algorithm in array

--- script.js
+++ script.js
@@ -916,4 +916,82 @@
 // str.push("mishra"+"mishr")
 // console.log(str);
 
+// let arr = [1, 2, 3, 4, 5];
 
+// for (let j = 0; j < 3; j++) {
+//   let copy = arr[0];
+//   for (let i = 0; i < arr.length - 1; i++) {
+//     arr[i] = arr[i + 1];
+//   }
+//   arr[arr.length - 1] = copy;
+// }
+
+// console.log(arr);
+
+// let arr = [1, 2, 3, 4, 5];
+
+// for (let j = 0; j < 1; j++) {
+//   let copy = arr[arr.length - 1];
+
+//   for (let i = arr.length - 1; i > 0; i--) {
+//     arr[i] = arr[i - 1];
+//   }
+
+//   arr[0] = copy;
+// }
+// console.log(arr);
+
+// let arr = [1,2,3,4,5];
+
+// let temp = new Array(arr.length);
+// let k = 1 % arr.length;
+// for (let i = 0; i < arr.length; i++) {
+//   temp[i] = arr[(i + k) % arr.length];
+// }
+
+// console.log(temp);
+
+// let arr = [1, 2, 3, 4, 5];
+// let temp = new Array(arr.length);
+// let k = 2 % arr.length;
+// for (let i = 0; i < arr.length; i++) {
+//   temp[(i + k) % arr.length] = arr[i];
+// }
+
+// console.log(temp);
+
+let arr = [1, 2, 3, 4, 5];
+let k = 1 % arr.length;
+reverse(0, k - 1);
+reverse(k, arr.length - 1);
+reverse(0, arr.length - 1);
+
+console.log(arr);
+
+function reverse(i, j) {
+  while (i < j) {
+    let temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+    i++;
+    j--;
+  }
+}
+
+let arr = [1, 2, 3, 4, 5];
+let k = 2 % arr.length;
+reverse(0, arr.length - 1);
+reverse(k, arr.length - 1);
+reverse(0, k - 1);
+
+console.log(arr);
+
+function reverse(i, j) {
+  while (i < j) {
+    let temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+    i++;
+    j--;
+  }
+}
